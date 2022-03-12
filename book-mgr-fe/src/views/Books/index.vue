3e92d9555e9a49b525c92166c37813941ecce08b
unknown <1578094091@qq.com>
2022-03-12 18:27:21 +0800

    前端图书管理基础界面

--- book-mgr-fe/src/views/Books/index.vue
+++ book-mgr-fe/src/views/Books/index.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    books
+    <a-card>
+      <h2>图书列表</h2>
+
+      <a-divider></a-divider>
+
+      <space-between>
+        <div class="search">
+          <a-input-search
+            placeholder="根据书名搜索"
+            enter-button
+          />
+        </div>
+        <a-button>添加一条</a-button>
+      </space-between>
+
+      <a-divider></a-divider>
+    
+      <a-table :columns="columns" :data-Source="dataSource">
+      </a-table>
+    </a-card>
   </div>
 </template>
+
+<script src="./index.js"></script>
+
+<!-- scoped为了防止样式污染 -->
+<style lang="scss" scoped>
+  @import './index.scss';
+</style>
